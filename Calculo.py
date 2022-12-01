b4624c9936b5a392d65c9ae27c986ca5ed6f517d
ArturEduardoLM <arturoleon77706@gmail.com>
2022-11-30 21:37:12 -0600

    VERSION 2 DEL PROYECTO

--- Calculo.py
+++ Calculo.py
@@ -11,3 +11,17 @@ while contador <= gradoPolinomio and contador>=0:
     coeficientesPolinomio.insert(contadorc, exponenteM)
     contadorc+=1
     contador-=1
+
+polinomio = 0;
+contadorf = 0;
+exponente = gradoPolinomio
+x = int(input("Ingresa el valor de x: "))
+while contadorf <= gradoPolinomio:
+    coeficiente = coeficientesPolinomio[contadorf]
+    funcion = (int(coeficiente)*int(x))**int(exponente)
+    polinomio = polinomio + int(funcion)
+    contadorf+=1
+    exponente-=1
+    
+print("Los coeficientes son: " + str(coeficientesPolinomio))
+print("EL resultado del polinomio evaluado en " + "(" + str(x) + ")" + " es: " + str(polinomio) )
