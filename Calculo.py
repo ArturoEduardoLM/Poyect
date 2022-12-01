a740971e8b428c69edf70cca126e40c0f2eda9d5
ArturEduardoLM <arturoleon77706@gmail.com>
2022-11-30 21:33:39 -0600

    Declarando las variables de una forma mas entendible

--- Calculo.py
+++ Calculo.py
@@ -1,25 +1,13 @@
-Polinomiogra = 0
-coeficientesPoli = []
+gradoPolinomio = 0
+coeficientesPolinomio = []
 coeficientes = 0
 contador= 0
-contador2 = 0
+contadorc = 0
 
-Polinomiogra = int(input("Ingresa el valor mas alto del polinomio: "))
-contador = Polinomiogra
-while contador <= Polinomiogra and contador>=0:
-    exponenteM = input("Coeficiente y el grado " + str(contador) + " :")
-    Polinomio.insert(contadorc, exponenteM)
-    contador2+=1
+gradoPolinomio = int(input("Ingresa el valor máximo del polinomio: "))
+contador = gradoPolinomio
+while contador <= gradoPolinomio and contador>=0:
+    exponenteM = input("Coeficiente del grado " + str(contador) + " :")
+    coeficientesPolinomio.insert(contadorc, exponenteM)
+    contadorc+=1
     contador-=1
-polinomio = 0;
-contadorf = 0;
-exponente = Polinomiogra
-x = int(input("Ingresa el valor de x: "))
-while contadorf <= gradoPolinomio:
-    coeficiente = coeficientesPoli[contadorf]
-    funcion = (int(coeficiente)*int(x))**int(exponente)
-    polinomio = polinomio + int(funcion)
-    contadorf+=1
-    exponente-=1
-    print("Los coeficientes son: " + str(coeficientesPoli))
-print("EL resultado del polinomio evaluado en " + "(" + str(x) + ")" + " es: " + str(polinomio) )
