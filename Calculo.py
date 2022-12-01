42dabdd6af56bc9de3e697face6ac0435e75c782
ArturEduardoLM <arturoleon77706@gmail.com>
2022-11-30 21:40:29 -0600

    Decidi ocupar la segunda version del poyecto porque es mas legible

--- Calculo.py
+++ Calculo.py
@@ -1,6 +1,27 @@
-Polinomio = 0
-coeficientesPoli = []
+gradoPolinomio = 0
+coeficientesPolinomio = []
 coeficientes = 0
 contador= 0
-contador2 = 0
+contadorc = 0
 
+gradoPolinomio = int(input("Ingresa el valor máximo del polinomio: "))
+contador = gradoPolinomio
+while contador <= gradoPolinomio and contador>=0:
+    exponenteM = input("Coeficiente del grado " + str(contador) + " :")
+    coeficientesPolinomio.insert(contadorc, exponenteM)
+    contadorc+=1
+    contador-=1
+
+polinomio = 0;
+contadorf = 0;
+exponente = gradoPolinomio
+x = int(input("Ingresa el valor de x: "))
+while contadorf <= gradoPolinomio:
+    coeficiente = coeficientesPolinomio[contadorf]
+    funcion = (int(coeficiente)*int(x))**int(exponente)
+    polinomio = polinomio + int(funcion)
+    contadorf+=1
+    exponente-=1
+    
+print("Los coeficientes son: " + str(coeficientesPolinomio))
+print("EL resultado del polinomio evaluado en " + "(" + str(x) + ")" + " es: " + str(polinomio) )
