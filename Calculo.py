d1afbae627f8fbcfefab92fdebc70536552ea09e
ArturEduardoLM <arturoleon77706@gmail.com>
2022-11-30 21:19:49 -0600

    Cambio de nombre de variables y sintaxys

--- Calculo.py
+++ Calculo.py
@@ -1,6 +1,24 @@
-Polinomio = 0
+Polinomiogra = 0
 coeficientesPoli = []
 coeficientes = 0
 contador= 0
 contador2 = 0
 
+Polinomiogra = int(input("Ingresa el valor mas alto del polinomio: "))
+contador = Polinomiogra
+while contador <= Polinomiogra and contador>=0:
+    exponenteM = input("Coeficiente y el grado " + str(contador) + " :")
+    Polinomio.insert(contadorc, exponenteM)
+    contador2+=1
+    contador-=1
+polinomio = 0;
+contadorf = 0;
+exponente = Polinomiogra
+x = int(input("Ingresa el valor de x: "))
+while contadorf <= gradoPolinomio:
+    coeficiente = coeficientesPoli[contadorf]
+    funcion = (int(coeficiente)*int(x))**int(exponente)
+    polinomio = polinomio + int(funcion)
+    contadorf+=1
+    exponente-=1
+    
