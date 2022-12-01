6546fd9cca3e6ad84c8e4002551dc0363cf77644
ArturoEduardoLM <113371865+ArturoEduardoLM@users.noreply.github.com>
2022-11-30 21:54:00 -0600

    Update Calculo.py
    ocasionaba fallo

--- Calculo.py
+++ Calculo.py
@@ -16,12 +16,3 @@ polinomio = 0;
 contadorf = 0;
 exponente = gradoPolinomio
 x = int(input("Ingresa el valor de x: "))
-while contadorf <= gradoPolinomio:
-    coeficiente = coeficientesPolinomio[contadorf]
-    funcion = (int(coeficiente)*int(x))**int(exponente)
-    polinomio = polinomio + int(funcion)
-    contadorf+=1
-    exponente-=1
-    
-print("Los coeficientes son: " + str(coeficientesPolinomio))
-print("EL resultado del polinomio evaluado en " + "(" + str(x) + ")" + " es: " + str(polinomio) )
